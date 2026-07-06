e901cfa85b5be9f7c13d2ae81d651a0a14984ccb
Supremo <103901456+miau4@users.noreply.github.com>
2026-07-06 04:50:29 -0300

    Update Install modulo atlas v5.sh

--- Install modulo atlas v5.sh
+++ Install modulo atlas v5.sh
@@ -10,7 +10,7 @@
 set -euo pipefail
 
 # ────────────────────────────────────────────────────────────
-# curl -fsSL https://raw.githubusercontent.com/miau4/Modulo-Atlas-Netsimon-Final/main/install_modulo_atlas_v5.sh -o /tmp/install_v5.sh
+# curl -fsSL "https://raw.githubusercontent.com/miau4/Modulo-Atlas-Netsimon-Final/main/Install%20modulo%20atlas%20v5.sh" -o /tmp/install_v5.sh && sudo bash /tmp/install_v5.sh
 # ────────────────────────────────────────────────────────────
 
 # ────────────────────────────────────────────────────────────
